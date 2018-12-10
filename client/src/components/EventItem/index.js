64686af3875bd70c5d22a78739067f921b758a64
steverodri91 <steve20.pro@gmail.com>
2018-12-10 16:24:53 -0500

    added class names to event-item

--- client/src/components/EventItem/index.js
+++ client/src/components/EventItem/index.js
@@ -5,11 +5,11 @@ const moment = require('moment');
 export default function EventItem(props){
   const event = props.event;
   return (
-    <div onClick={props.handleEventSelect}>
-      <img src="" alt="event-image"/>
-      <h5>{event.title}</h5>
-      <p>{moment(event.start_date).format('llll')}</p>
-      <p>{event.address}</p>
+    <div className="event-item" onClick={props.handleEventSelect}>
+      <img className="event-item-image" src="" alt="event-image"/>
+      <h5 className="event-item-title">{event.title}</h5>
+      <p className='event-item-start_date'>{moment(event.start_date).format('llll')}</p>
+      <p className='event-item-address'>{event.address}</p>
     </div>
   )
 }
