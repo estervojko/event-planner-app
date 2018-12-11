35dea6201fe0c71ee66b0ace951f2087420b03b6
steverodri91 <steve20.pro@gmail.com>
2018-12-11 14:33:06 -0500

    added postEvent to eventReq

--- client/src/AJAXRequests/eventReq.js
+++ client/src/AJAXRequests/eventReq.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const moment = require('moment');
 
 const BASE_URL = 'http://localhost:3000/';
 const EVENTS = 'events/';
@@ -21,13 +22,29 @@ const eventReq = {
     } catch (e) {
       console.log("Axios - Event req - failed to GET ONE event");
     }
+  },
+  postEvent: async function (data){
+    try {
+      const resp = await axios.post(BASE_URL + EVENTS, data);
+      const event = resp.data.event;
+      return event;
+    } catch (e) {
+      console.log("Axios - Event req - failed to POST event");
+    }
   }
 }
 
 
 async function test(){
+  const data = {
+    title: "Test Event from Axios",
+    description: "Description...",
+    start_date: moment().format(),
+    end_date: moment().add(2, 'hours').format(),
+    address: "address"
+  }
   try {
-    const resp = await eventReq.getEvent(2);
+    const resp = await eventReq.postEvent(data);
     console.log(resp);
   } catch (e) {
     console.log(e);
