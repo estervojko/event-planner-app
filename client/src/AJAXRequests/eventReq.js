eee5171ff8eee3a3a89b14802743d1048b19dd84
steverodri91 <steve20.pro@gmail.com>
2018-12-11 14:42:45 -0500

    added putEvent to eventReq

--- client/src/AJAXRequests/eventReq.js
+++ client/src/AJAXRequests/eventReq.js
@@ -5,31 +5,54 @@ const BASE_URL = 'http://localhost:3000/';
 const EVENTS = 'events/';
 
 const eventReq = {
+
   getEvents: async function (){
     try {
       const resp = await axios.get(BASE_URL + EVENTS);
       const events = resp.data.events;
       return events;
     } catch (e) {
-      console.log("Axios - Event req - failed to GET ALL events");
+      console.log("Axios - Event req - failed to GET ALL events ", e.message);
     }
   },
+
   getEvent: async function (id){
     try {
       const resp = await axios.get(BASE_URL + EVENTS + id);
       const event = resp.data.event;
       return event;
     } catch (e) {
-      console.log("Axios - Event req - failed to GET ONE event");
+      console.log("Axios - Event req - failed to GET ONE event ", e.message);
     }
   },
+
   postEvent: async function (data){
     try {
       const resp = await axios.post(BASE_URL + EVENTS, data);
       const event = resp.data.event;
       return event;
     } catch (e) {
-      console.log("Axios - Event req - failed to POST event");
+      console.log("Axios - Event req - failed to POST event ", e.message);
+    }
+  },
+
+  deleteEvent: async function (id) {
+    try {
+      const resp = await axios.delete(BASE_URL + EVENTS + id);
+      const event = resp.data.event;
+      return event;
+    } catch (e) {
+      console.log("Axios - Event req - failed to POST event ", e.message);
+    }
+  },
+
+  putEvent: async function (id, data) {
+    try {
+      const resp = await axios.put(BASE_URL + EVENTS + id, data);
+      const event = resp.data.updatedEvent;
+      return event;
+    } catch (e) {
+      console.log("Axios - Event req - failed to PUT event ", e.message);
     }
   }
 }
@@ -37,14 +60,14 @@ const eventReq = {
 
 async function test(){
   const data = {
-    title: "Test Event from Axios",
+    title: "Update Event from Axios",
     description: "Description...",
     start_date: moment().format(),
-    end_date: moment().add(2, 'hours').format(),
+    end_date: moment().add(3, 'hours').format(),
     address: "address"
   }
   try {
-    const resp = await eventReq.postEvent(data);
+    const resp = await eventReq.putEvent(4, data);
     console.log(resp);
   } catch (e) {
     console.log(e);
