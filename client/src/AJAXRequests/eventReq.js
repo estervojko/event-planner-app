0bfe15d7a96c96367a6f5dc4b33fd35ac052f7b6
steverodri91 <steve20.pro@gmail.com>
2018-12-11 14:23:24 -0500

    added getEvent to eventReq

--- client/src/AJAXRequests/eventReq.js
+++ client/src/AJAXRequests/eventReq.js
@@ -1,16 +1,25 @@
 const axios = require('axios');
 
 const BASE_URL = 'http://localhost:3000/';
-const EVENTS = 'events';
+const EVENTS = 'events/';
 
 const eventReq = {
   getEvents: async function (){
     try {
-      const resp = await axios.get(BASE_URL + EVENTS)
-      const events = resp.data.events
-      return events
+      const resp = await axios.get(BASE_URL + EVENTS);
+      const events = resp.data.events;
+      return events;
     } catch (e) {
       console.log("Axios - Event req - failed to GET ALL events");
+    }
+  },
+  getEvent: async function (id){
+    try {
+      const resp = await axios.get(BASE_URL + EVENTS + id);
+      const event = resp.data.event;
+      return event;
+    } catch (e) {
+      console.log("Axios - Event req - failed to GET ONE event");
     }
   }
 }
@@ -18,12 +27,10 @@ const eventReq = {
 
 async function test(){
   try {
-    const resp = await eventReq.getEvents();
+    const resp = await eventReq.getEvent(2);
     console.log(resp);
   } catch (e) {
     console.log(e);
-  } finally {
-
   }
 }
 
