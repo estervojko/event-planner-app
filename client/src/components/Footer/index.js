74fd6136185078e0375ab6df7d93fce01f2c9c4f
Christopher Laeri <claeri27@gmail.com>
2018-12-12 00:46:27 -0500

    added SplashNew and styles

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,8 +4,8 @@ import './index.css';
 export default function Footer(){
     return(
       <div className="footer">
-      <p>Corporate BS</p>
-      <p>FAQ</p>
+      <p className="footer-text1">Corporate BS</p>
+      <p className="footer-text2">FAQ</p>
       </div>
     )
 }
