171e709ae7fc91e2fcfa630f6f87eb796de6669a
Christopher Laeri <claeri27@gmail.com>
2018-12-12 23:13:11 -0500

    Ignore this commit

--- client/src/components/EventDetail/index.js
+++ client/src/components/EventDetail/index.js
@@ -31,7 +31,7 @@ export default class EventDetail extends React.Component{
     const event = this.props.event;
     return (
       <div className="event-detail">
-        <img className="event-detail-image" src="https://food.fnr.sndimg.com/content/dam/images/food/fullset/2012/7/25/0/FNM_090112-Mix-and-Match-Classic-Cookies-Recipe-03_s4x3.jpg.rend.hgtvcom.616.462.suffix/1371607143890.jpeg" alt="event" />
+        <img className="event-detail-image" src={event.img} alt="EventDetail Img Not Found" />
         <div className="event-detail-text-container">
           <h3 className="event-detail-title">{event.title}</h3>
           <h5 className="event-detail-organizer">{event.organizer}</h5>
