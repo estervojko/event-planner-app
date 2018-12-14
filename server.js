11c25c66d37da6f2de46eac3c6ec45d7f0c0a908
Ester Vojkollari <e.vojko@gmail.com>
2018-12-14 10:01:54 -0500

    heroku changes

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const { userRouter } = require('./routes/user');
 
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use(cors());
